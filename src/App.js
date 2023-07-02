eed5a8c31945fbad765c490a216a43c0b7a189b8
Olena Litovska <litovskaolena@gmail.com>
2023-07-02 11:50:44 +0300

    Added lodo and set up the firebase, also added 'Sign In' page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in-component";
 import Navigation from "./components/navigation/navigation.component";
-//084
+
+//086
 const Shop = () =>{
     return <h1>I am the shop page</h1>;
 }
@@ -13,6 +15,7 @@ const App = () => {
             <Route path='/' element={<Navigation/>}>
                 <Route index element={<Home/>}/>
                 <Route path='shop' element={<Shop/>}/>
+                <Route path='sign-in' element={<SignIn/>}/>
             </Route>
         </Routes>
     );
